d1d205db1c4dad20156f34369403940199cb4959
mysticPrg <mysticPrg@gmail.com>
2014-10-16 05:09:50 +0900

    null pointer check

--- src/DBHelper.js
+++ src/DBHelper.js
@@ -4,29 +4,31 @@
 
 var DBHelper = function DBHelper() {
 //	this.server = new mongodb.Server('localhost', 27017, {auto_reconnect: true});
-	this.client = require('mongodb').MongoClient;
-	this.db = null;
-	this.url = 'mongodb://localhost:27017/careerbox';
+    this.client = require('mongodb').MongoClient;
+    this.db = null;
+    this.url = 'mongodb://localhost:27017/careerbox';
 };
 
 DBHelper.prototype.connect = function connect(callback) {
-	if ( this.db !== null ) {
-		callback(null, this.db);
-		return;
-	}
+    if (this.db !== null) {
+        callback(null, this.db);
+        return;
+    }
 
-	var self = this;
+    var self = this;
 
-	this.client.connect(this.url, function(err, db) {
-		console.log('connected to mongo db');
-		self.db = db;
-		callback(err, db);
-	});
+    this.client.connect(this.url, function (err, db) {
+        console.log('connected to mongo db');
+        self.db = db;
+        callback(err, db);
+    });
 };
 
 DBHelper.prototype.close = function close() {
-	this.db.close();
-	this.db = null;
-}
+    if (this.db) {
+        this.db.close();
+        this.db = null;
+    }
+};
 
 module.exports = DBHelper;
