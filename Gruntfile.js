83d9bbc299e938f1067d6b56568319797c40f334
mysticPrg <mysticPrg@gmail.com>
2014-10-31 18:00:25 +0900

    Merge branch 'master' of https://github.com/mysticPrg/CareerBoxServer
    Conflicts:
    	Gruntfile.js
    	reports/coverage/coverage.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,7 @@ module.exports = function (grunt) {
         }
     });
 
+    grunt.loadNpmTasks("grunt-jscpd");
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-contrib-jshint");
