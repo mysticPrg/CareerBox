af20e1c64115868a1ffa6e3bbe2d461ed2d7be0c
JEONGBORAM-PC-W1 <shelter.sune@gmail.com>
2014-12-12 00:36:17 +0900

    [보람] 페이지 템플릿 적용

--- res/app/js/controllers/informationManager.js
+++ res/app/js/controllers/informationManager.js
@@ -17,7 +17,11 @@ define([
     'component/information/computerAbilityInformation/component',
     'component/information/paperAbilityInformation/component',
     'component/information/scholarshipInformation/component',
-    'component/information/awardInformation/component'
+    'component/information/awardInformation/component',
+    'component/information/localActivityInformation/component',
+    'component/information/globalActivityInformation/component',
+    'component/information/projectInformation/component',
+    'component/information/columnInformation/component'
 ], function ($, ng, app) {
     app.controller('informationManager', ['$scope', '$http', function ($scope, $http) {
         $scope.initialize = function () {
