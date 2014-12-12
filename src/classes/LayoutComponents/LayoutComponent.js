489c531da0a2f7e216456501d4034e6903df3723
mysticPrg <mysticPrg@gmail.com>
2014-12-12 16:01:06 +0900

    [현구] 바뀐 구조 다시 적용

--- src/classes/LayoutComponents/LayoutComponent.js
+++ src/classes/LayoutComponents/LayoutComponent.js
@@ -9,22 +9,30 @@ if (typeof define !== 'function') {
 define([
     'classes/Structs/Size',
     'classes/Structs/Position',
+    'classes/Structs/Outline',
+    'classes/Structs/Fill',
     'classes/Enums/LayoutComponentType'
 ], function (Size, Position, Outline, Fill, LayoutComponentType) {
 
     function LayoutComponent(props) {
         this._id = null;
+        this.layoutComponentType = LayoutComponentType.item;
         this.zOrder = 0;
         this.size = new Size();
         this.pos = new Position();
-        this.layoutComponentType = LayoutComponentType.item;
+        this.fill = new Fill();
+        this.outline = new Outline();
+        this.radius = 0;
 
         if (props) {
             this._id = props._id ? props._id : this._id;
+            this.layoutComponentType = props.layoutComponentType ? props.layoutComponentType : this.layoutComponentType;
             this.zOrder = props.zOrder ? props.zOrder : this.zOrder;
             this.size = props.size ? props.size : this.size;
             this.pos = props.pos ? props.pos : this.pos;
-            this.layoutComponentType = props.layoutComponentType ? props.layoutComponentType : this.layoutComponentType;
+            this.fill = props.fill ? props.fill : this.fill;
+            this.outline = props.outline ? props.outline : this.outline;
+            this.radius = props.radius ? props.radius : this.radius;
         }
     };
 
