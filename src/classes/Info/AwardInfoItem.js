c409da253c3ca0dd1c33cba4b95bfe2521357f60
ByoungJin <kbj2043@gmail.com>
2014-12-18 02:59:30 +0900

    [병진] info class L_ >> S_

--- src/classes/Info/AwardInfoItem.js
+++ src/classes/Info/AwardInfoItem.js
@@ -14,7 +14,7 @@ define([], function () {
         'S_title' : '작품명',
         'S_host'    : '주최기관',
         'D_date'   : '수상일자',
-        'L_detail'  : '상세정보'
+        'S_detail'  : '상세정보'
     };
 
     function AwardInfoItem(props) {
@@ -23,14 +23,14 @@ define([], function () {
         this.S_title = ''; // 작품명
         this.S_host = ''; // 주최기관
         this.D_date = new Date(); // 수상일자
-        this.L_detail = ''; // 상세정보
+        this.S_detail = ''; // 상세정보
 
         if ( props ) {
             this.S_name = props.S_name ? props.S_name : this.S_name;
             this.S_title = props.S_title ? props.S_title : this.S_title;
             this.S_host = props.S_host ? props.S_host : this.S_host;
             this.D_date = props.D_date ? props.D_date : this.D_date;
-            this.L_detail = props.L_detail ? props.L_detail : this.L_detail;
+            this.S_detail = props.S_detail ? props.S_detail : this.S_detail;
         }
 
         AwardInfoItem.prototype.getAttributeName = function getAttributeName(key){
