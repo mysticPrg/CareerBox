468fed0e473edba205035df446455b36ebeca78d
mysticPrg <mysticPrg@gmail.com>
2014-12-17 23:05:50 +0900

    [현구] File 다운로드 서비스 작성

--- src/services/UploadServicer.js
+++ src/services/UploadServicer.js
@@ -10,17 +10,36 @@ var ServiceUtil = require('../util/ServiceUtil');
 var Result = require('./result');
 var ObjectID = require('mongodb').ObjectID;
 
+var path = require('path');
+var mime = require('mime');
+var fs = require('fs');
+
+var fileDir = __dirname + '/../../res/uploads/file';
+
 var uploader = new Uploader({
     debug: true,
     validate: true,
     safeName: true,
     publicDir: __dirname + '/../../res',
-    uploadDir: __dirname + '/../../res/uploads/file',
+    uploadDir: fileDir,
     uploadUrl: '/upload/file'
 });
 
 function checkArgForFiles(req, res) {
     if (!req.files) {
+
+        var result = new Result(null);
+        result.setCode('001');
+        res.end(result.toString());
+
+        return false;
+    }
+
+    return true;
+}
+
+function checkArgForId(req, res) {
+    if (!req.params._id) {
 
         var result = new Result(null);
         result.setCode('001');
@@ -59,6 +78,31 @@ function uploadService(req, res) {
     });
 }
 
+function downloadService(req, res) {
+    if (!ServiceUtil.checkSession(req, res)) {
+        return;
+    }
+    if (!checkArgForId(req, res)) {
+        return;
+    }
+
+    FileDB.read(req.params._id, function(err, finded) {
+
+        var filepath = fileDir + '/' + finded.name;
+//        var filepath = path.basename(filename);
+        var mimetype = mime.lookup(finded.name); //(filepath);
+
+        res.setHeader('Content-disposition', 'attachment; filename=' + finded.originalName);
+        res.setHeader('Content-type', mimetype);
+
+        var filestream = fs.createReadStream(filepath);
+        filestream.pipe(res);
+    });
+}
+
 module.exports.set = function (server) {
-    server.post('/upload/file', multipart, uploadService);
+    server.post('/file/upload', multipart, uploadService);
+    server.get('/file/download/:_id', downloadService);
+//    server.get('/file/list', getList);
+//    server.delete('/file/delete', deleteService);
 };
