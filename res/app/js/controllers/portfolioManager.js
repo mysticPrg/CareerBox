d9a589c46ab2e787fdf92a983fddbd98b932b09d
JEONGBORAM-PC-W1 <shelter.sune@gmail.com>
2014-12-18 03:17:21 +0900

    [보람] Portfolio Manager 수정~

--- res/app/js/controllers/portfolioManager.js
+++ res/app/js/controllers/portfolioManager.js
@@ -6,10 +6,22 @@ define([
     'angular',
     'app',
     '../../component/menu/component',
-    '../../component/newPortfolio/component'
+    '../../component/newPortfolio/component',
+    'services/EditorData',
+    'classes/Info/PersonalInfo'
+], function ($, ng, app, EditorData, PersonalInfo) {
+    app.controller('portfolioManager', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+        $scope.userInfo = '';
 
-], function ($, ng, app) {
-    app.controller('portfolioManager', ['$scope', '$http', function ($scope, $http) {
+        $scope.initialize = function (){
+            $http.get('http://210.118.74.166:8123/info/personal', {withCredentials: true}).
+                success(function(data, status, headers, config) {
+                    $scope.userInfo = data.result;
+                });
+        }
 
+        $scope.editPersonalInformation = function(){
+            $window.location.href = 'InformationManager.html';
+        }
     }]);
 });
