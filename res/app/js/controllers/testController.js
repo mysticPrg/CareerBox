728e83fbd845fd73b6d51677316fde7100651604
Boram Jeong <shelter.sune@gmail.com>
2014-12-18 00:09:07 +0900

    [보람] 데이터 바인딩 - get available attribute service 작업

--- res/app/js/controllers/testController.js
+++ res/app/js/controllers/testController.js
@@ -5,9 +5,39 @@ define([
     'jquery',
     'angular',
     'app',
-    'classes/Enums/InfoCategory'
+    'classes/Enums/InfoCategory',
+    'services/getAvailableAttribute'
 ], function ($, ng, app, InfoCategory) {
-    app.controller('testController', ['$scope', '$http', function ($scope, $http) {
+    app.controller('testController', ['$scope', 'getAvailableAttribute', function ($scope, getAvailableAttribute) {
         $scope.infoCategory = InfoCategory;
+        $scope.category = '';
+
+        $scope.types = ['string', 'number', 'boolean', 'file', 'image', 'term', 'date'];
+        $scope.type;
+
+        $scope.result;
+
+        $scope.test = function (){
+            var category = $scope.category.infoType;
+
+            var infoType = '';
+            if($scope.type === $scope.types[0]){
+                infoType = 'S';
+            }else if($scope.type === $scope.types[1]){
+                infoType = 'N';
+            }else if($scope.type === $scope.types[2]){
+                infoType = 'B';
+            }else if($scope.type === $scope.types[3]){
+                infoType = 'F';
+            }else if($scope.type === $scope.types[4]){
+                infoType = 'I';
+            }else if($scope.type === $scope.types[5]){
+                infoType = 'T';
+            }else if($scope.type === $scope.types[6]){
+                infoType = 'D';
+            }
+
+            $scope.result = getAvailableAttribute(category, infoType);
+        }
     }]);
 });
---
+++ res/app/js/services/getAvailableAttribute.js
@@ -0,0 +1,70 @@
+/**
+ * Created by KHW on 2014-12-17.
+ */
+define([
+    'app',
+    'classes/Enums/InfoType',
+    'classes/Info/PersonalInfo',
+    'classes/Info/AdditionalInfo',
+    'classes/Info/HighSchoolInfoItem',
+    'classes/Info/UnivSchoolInfoItem',
+    'classes/Info/WorkingInfoItem',
+    'classes/Info/CertificationAbilityInfoItem',
+    'classes/Info/ComputerAbilityInfoItem',
+    'classes/Info/ProficiencyInfoItem',
+    'classes/Info/ScholarshipInfoItem',
+    'classes/Info/AwardInfoItem',
+    'classes/Info/LocalActivityInfoItem',
+    'classes/Info/GlobalActivityInfoItem',
+    'classes/Info/ProjectInfoItem',
+    'classes/Info/ColumnInfoItem'
+], function (app, InfoType, PersonalInfo, AdditionalInfo, HighSchoolInfoItem, UnivSchoolInfoItem, WorkingInfoItem, CertificationAbilityInfoItem, ComputerAbilityInfoItem,
+             ProficiencyInfoItem, ScholarshipInfoItem, AwardInfoItem, LocalActivityInfoItem, GlobalActivityInfoItem, ProjectInfoItem, ColumnInfoItem) {
+    app.factory('getAvailableAttribute', function () {
+        return function (category, infoType) {
+            var result = [];
+            var item = getItem(category);
+
+            for(var key in item){
+                if(key.split('_')[0] === infoType){
+                    result.push(key);
+                }
+            }
+
+            return result;
+        }
+
+        function getItem(category){
+            switch (category){
+                case InfoType.personalInfo:
+                    return new PersonalInfo();
+                case InfoType.additionalInfo:
+                    return new AdditionalInfo();
+                case InfoType.highSchoolInfo:
+                    return new HighSchoolInfoItem();
+                case InfoType.univSchoolInfo:
+                    return new UnivSchoolInfoItem();
+                case InfoType.workingInfo:
+                    return new WorkingInfoItem();
+                case InfoType.certificationAbilityInfo:
+                    return new CertificationAbilityInfoItem();
+                case InfoType.computerAbilityInfo:
+                    return new ComputerAbilityInfoItem();
+                case InfoType.proficiencyInfo:
+                    return new ProficiencyInfoItem();
+                case InfoType.scholarshipInfo:
+                    return new ScholarshipInfoItem();
+                case InfoType.awardInfo:
+                    return new AwardInfoItem();
+                case InfoType.localActivityInfo:
+                    return new LocalActivityInfoItem();
+                case InfoType.globalActivityInfo:
+                    return new GlobalActivityInfoItem();
+                case InfoType.projectInfo:
+                    return new ProjectInfoItem();
+                case InfoType.columnInfo:
+                    return new ColumnInfoItem();
+            }
+        }
+    });
+});
