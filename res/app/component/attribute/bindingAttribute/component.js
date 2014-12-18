7f86d397b72b78fc7d46351b9c225b065578b613
ByoungJin <kbj2043@gmail.com>
2014-12-19 05:02:14 +0900

    [병진] 템플릿에서 데이터 바인딩

--- res/app/component/attribute/bindingAttribute/component.js
+++ res/app/component/attribute/bindingAttribute/component.js
@@ -2,37 +2,50 @@
 define([
     'app',
     'classes/Enums/InfoCategory',
-    'services/getAvailableAttribute',
-    'services/getAvailableAttribute'
+    'services/getAttributeNames',
+    'services/EditorData'
 ], function (app, InfoCategory) {
 
-    app.directive('bindingAttribute', function (getAvailableAttribute) {
+    app.directive('bindingAttribute', function (getAttributeNames, EditorData) {
         return {
             restrict: 'A',
             scope: {
                 data : "=to",
                 layoutType : "=type"
+
             },
             templateUrl: require.toUrl('component/attribute/bindingAttribute/template.html'),
             link: function ($scope, element, att) {
                 console.log('binding Attribute $scope', $scope);
+
                 $scope.infoCategory = InfoCategory;
                 $scope.category = '';
-
-                $scope.types = ['string', 'number', 'boolean', 'file', 'image', 'term', 'date'];
-                $scope.type;
-
-                $scope.result;
-
                 $scope.setCategory = function(){
                     // 템플릿에 카테고리를 매칭
                     $scope.data.bindingType = $scope.category;
-                    console.log('$scope.data.bindingType', $scope.data.bindingType);
-                }
+                };
 
                 $scope.clear = function() {
                     $scope.data.bindingType = '';
-                }
+                };
+
+                // 속성 이름들 모두 가져오기
+                $scope.EditorData = EditorData;
+                $scope.$watch('EditorData.focusId',function(){
+                    $scope.attributeNames = getAttributeNames(EditorData.template.target.bindingType.infoType);    // 상위 아티클의 바인딩타입이 들어가야함.
+                },true);
+
+                $scope.setAttributeName = function() {
+                    // 아이템에 속성이름을 매칭
+                    for(var key in $scope.attributeNames){
+                        if($scope.attributeNames[key] == $scope.attributeName){
+//                            console.log('key', key)
+//                            console.log('EditorData.template.target.bindingType', EditorData.template.target.bindingType)
+
+                            $scope.data.bindingType = EditorData.template.target.bindingType[key];
+                        }
+                    }
+                };
             }
         };
     });
