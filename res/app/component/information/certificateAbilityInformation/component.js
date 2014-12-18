1ac3104fd492d02c269c7ff6eee5115e091262cb
JEONGBORAM-PC-W1 <shelter.sune@gmail.com>
2014-12-18 16:34:56 +0900

    [보람] 자격증 파일 업로드 모듈 구현

--- res/app/component/information/certificateAbilityInformation/component.js
+++ res/app/component/information/certificateAbilityInformation/component.js
@@ -5,10 +5,13 @@
 define([
     'app',
     'services/InformationData',
-    'classes/Info/CertificationAbilityInfoItem'
+    'classes/Info/CertificationAbilityInfoItem',
+    'angular-upload'
 ], function (app, InformationData, CertificationAbilityInfoItem) {
-    app.controller('certificateAbilityInformationContorller', ['$scope', function ($scope) {
+    app.controller('certificateAbilityInformationContorller', ['$scope', '$upload', function ($scope, $upload) {
         $scope.certificateAbilityInfoItem = new CertificationAbilityInfoItem();
+        $scope.files;
+        $scope.progress = 0;
 
         $scope.InformationData = InformationData;
 
@@ -20,10 +23,45 @@ define([
             var newCertificateAbilityInfoItem = new CertificationAbilityInfoItem($scope.certificateAbilityInfoItem);
             $scope.certificateAbilityInfoItems.push(newCertificateAbilityInfoItem);
             $scope.certificateAbilityInfoItem = new CertificationAbilityInfoItem();
+
+            initializeFileForm();
+        }
+
+        function initializeFileForm(){
+            $('#progress-bar').css('display', 'none');
+            $('#file').val('');
         }
 
         $scope.delCertificateAbilityInfo = function (index) {
             $scope.certificateAbilityInfoItems.splice(index, 1);
+        }
+
+        $scope.onFileSelect = function ($files) {
+            $scope.files = $files;
+            $('#btn-upload').fadeIn('slow');
+        }
+
+        $scope.upload = function (){
+            $('#progress-bar').fadeIn('slow');
+
+            for (var i = 0; i < $scope.files.length; i++) {
+                var file = $scope.files[i];
+                $scope.upload = $upload.upload({
+                    url: 'http://210.118.74.166:8123/file',
+                    method: 'POST',
+                    withCredentials: true,
+                    data: {isBinding: true},
+                    file: file
+                }).progress(function (evt) {
+                    $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+                }).success(function (data, status, headers, config) {
+                    $scope.certificateAbilityInfoItem.F_file = data.result;
+                });
+            }
+        }
+
+        function uploadDone(){
+            $('#file-name').fadeIn('slow');
         }
 
     }]);
