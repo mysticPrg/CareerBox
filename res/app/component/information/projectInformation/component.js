6e8a158b84718e02724ffed310c91524347b09e2
JEONGBORAM-PC-W1 <shelter.sune@gmail.com>
2014-12-20 04:27:58 +0900

    [보람] 프로젝트 사진 업로드 구현

--- res/app/component/information/projectInformation/component.js
+++ res/app/component/information/projectInformation/component.js
@@ -6,54 +6,75 @@ define([
     'services/InformationData',
     'classes/Info/ProjectInfoItem',
     'angular-upload',
-    'services/fileUpload'
+    'services/fileUpload',
+    'services/ImageUpload'
 ], function (app, InformationData, ProjectInfoItem) {
-    app.controller('projectInformationController', ['$scope', '$upload', 'fileUpload', function ($scope, $upload, fileUpload) {
-        $scope.projectInfoItem = new ProjectInfoItem();
-        $scope.files;
-        $scope.progress = 0;
-
-        $scope.InformationData = InformationData;
-
-        $scope.$watch("InformationData.projectInfo", function () {
-            $scope.projectInfoItems = InformationData.projectInfo.items;
-        }, true);
-
-        function initializeFileForm(){
-            $scope.progress = 0;
-            $('#project_file').val('');
-            $('#project_upload').css('display', 'none');
-            $('#project_progressbar').css('display', 'none');
-        }
-
-        $scope.addProjectInfo = function () {
-            var newProjectInfoItem = new ProjectInfoItem($scope.projectInfoItem);
-            $scope.projectInfoItems.push(newProjectInfoItem);
+    app.controller('projectInformationController', ['$scope', '$upload', 'fileUpload', 'ImageUpload',
+        function ($scope, $upload, fileUpload, ImageUpload) {
             $scope.projectInfoItem = new ProjectInfoItem();
+            $scope.files;
+            $scope.progress = 0;
+            $scope.imageProgress = 0;
 
-            initializeFileForm();
-        }
+            $scope.InformationData = InformationData;
 
-        $scope.delProjectInfo = function (index) {
-            $scope.projectInfoItems.splice(index, 1);
-        }
+            $scope.$watch("InformationData.projectInfo", function () {
+                $scope.projectInfoItems = InformationData.projectInfo.items;
+            }, true);
 
-        $scope.onFileSelectProjectInfo = function ($files) {
-            $scope.files = $files;
-            $('#project_upload').fadeIn('slow');
-        }
+            function initializeFileForm() {
+                $scope.progress = 0;
+                $scope.imageProgress = 0;
 
-        $scope.uploadProjectInfo = function (){
-            $('#project_progressbar').fadeIn('slow');
+                $('#project_file').val('');
+                $('#project_upload').css('display', 'none');
+                $('#project_progressbar').css('display', 'none');
 
-            fileUpload($upload, $scope.files, true, function(evt){
-                $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-            },function(data){
-                $scope.projectInfoItem.F_file = data.result;
-            });
-        }
+                $('#project_picture_upload').val('');
+                $('#project_picture').attr('src', '../img/noImage200x200.png');
+                $('#project_picture_progressbar').css('display', 'none');
+            }
 
-    }]);
+            $scope.addProjectInfo = function () {
+                var newProjectInfoItem = new ProjectInfoItem($scope.projectInfoItem);
+                $scope.projectInfoItems.push(newProjectInfoItem);
+                $scope.projectInfoItem = new ProjectInfoItem();
+
+                initializeFileForm();
+            }
+
+            $scope.delProjectInfo = function (index) {
+                $scope.projectInfoItems.splice(index, 1);
+            }
+
+            $scope.onFileSelectProjectInfo = function ($files) {
+                $scope.files = $files;
+                $('#project_upload').fadeIn('slow');
+            }
+
+            $scope.uploadProjectInfo = function () {
+                $('#project_progressbar').fadeIn('slow');
+
+                fileUpload($upload, $scope.files, true, function (evt) {
+                    $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+                }, function (data) {
+                    $scope.projectInfoItem.F_file = data.result;
+                });
+            }
+
+            $scope.onFileSelectProjectImage = function ($files) {
+                $scope.imageProgress = 0;
+                $('#project_picture_progressbar').fadeIn('slow');
+
+                ImageUpload($upload, $files, 'symbol', function (evt) {
+                    $scope.imageProgress = parseInt(100.0 * evt.loaded / evt.total);
+                }, function (data) {
+                    $scope.projectInfoItem.I_image = data.result;
+                    $('#project_picture').attr('src', 'http://210.118.74.166:8123/image/symbol/thumb/' + data.result._id);
+                });
+            }
+
+        }]);
 
     app.directive('projectInformation', function () {
         return {
