fcc642f721f14993e4763b8d9d528c2ce90a3284
JEONGBORAM-PC-W1 <shelter.sune@gmail.com>
2014-12-21 02:49:33 +0900

    [보람] 사진 업로드 모달 수정

--- res/app/component/bindingImageModal/component.js
+++ res/app/component/bindingImageModal/component.js
@@ -7,46 +7,63 @@ define([
     'services/ImageUpload',
     'services/deleteImage'
 ], function (app, Paper) {
-    app.controller('bindingImageModal', function ($scope, $modalInstance, $upload, ImageUpload, $http, $q, deleteImage, $http) {
+    app.controller('bindingImageModal', function ($scope, $modalInstance, $compile, $upload, ImageUpload, $http, $q, deleteImage, $http) {
+        $scope.files;
+        $scope.progress = 0;
+
+        $scope.imageFile;
 
         // 기존 파일 가져오기
-        function getImageFiles(){
+        function getImageFiles() {
             var loadImagePromiss = $http.get('http://210.118.74.166:8123/image', {withCredentials: true});
             $q.all([loadImagePromiss]).then(function (resultArray) {
                 $scope.imageFiles = resultArray[0].data.result;
             });
         }
+
         getImageFiles();
 
-        $scope.progress = 0;
+        $scope.selectImage = function (imageFile, e) {
+            $scope.imageFile = imageFile;
 
-        $scope.onFileSelectProfileImage = function ($files) {
+            $('ul li div').removeClass('focus');
+            $(e.target).parent().addClass('focus');
+        }
+
+        $scope.onFileSelectUnbindImage = function ($files) {
+            $scope.files = $files;
+        };
+
+        $scope.uploadUnbindImage = function () {
             $('#image_picture_progressbar').fadeIn('slow');
 
-            ImageUpload($upload, $files, '', function (evt) {   // 독립이미지
+            ImageUpload($upload, $scope.files, '', function (evt) {   // 독립이미지
                 $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
             }, function (data) {
                 // 파일 다시 받기
                 getImageFiles();
-                alert('업로드가 완료 되었습니다.');
+
+                $scope.progress = 0;
+                $('#image_picture_file').val('');
+                $('#image_picture_progressbar').css('display', 'none');
             });
-        };
+        }
 
         ///////////////////////////////
 
-        $scope.paper = new Paper();
-        $scope.save = function (imageFile) {
-            $modalInstance.close(imageFile); // 인자에 넘길 것을 넣는다.
+//        $scope.paper = new Paper();
+        $scope.save = function () {
+            $modalInstance.close($scope.imageFile); // 인자에 넘길 것을 넣는다.
         };
         $scope.cancel = function () {
             $modalInstance.dismiss();
         };
 
-        $scope.deleteImage = function(id) {
-                deleteImage({'_id' : id}, function() {
-                    // 파일 다시 받기
-                    getImageFiles();
-                });
+        $scope.deleteImage = function (id) {
+            deleteImage({'_id': id}, function () {
+                // 파일 다시 받기
+                getImageFiles();
+            });
         }
     });
     return {
