d2d0d113e93ca9a9742ffd4f54ffeb4d299e3cda
JEONGBORAM-PC-W1 <shelter.sune@gmail.com>
2014-12-22 22:33:00 +0900

    [보람] personalInfo, additionalInfo 변경

--- res/app/js/controllers/portfolioManager.js
+++ res/app/js/controllers/portfolioManager.js
@@ -12,13 +12,10 @@ define([
 ], function ($, ng, app, EditorData, PersonalInfo) {
     app.controller('portfolioManager', ['$scope', '$http', '$window', function ($scope, $http, $window) {
         $scope.initialize = function (){
-            $http.get('http://210.118.74.166:8123/info/personal', {withCredentials: true}).
+            $http.get('http://210.118.74.166:8123/info/mainInfo', {withCredentials: true}).
                 success(function(data, status, headers, config) {
                     $scope.userInfo = data.result;
-
-                    if(data.result.S_name_kr === '' || data.result.S_name_kr === undefined){
-                        $scope.userInfo.S_name_kr = data.result._member_email.split('@')[0];
-                    }
+                    console.log(data.result);
 
                     $('#header-title').fadeIn('slow');
 
