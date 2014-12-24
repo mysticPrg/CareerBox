41daf63264886b30542e503a9c773406a45e048d
ByoungJin <kbj2043@gmail.com>
2014-12-25 02:10:52 +0900

    [병진] 아이템/아티클 배치한 후에 페이퍼 크기 줄일 때 아이템이 페이퍼 밖으로 나가는 상태로 줄여지지 않게 처리

--- res/app/js/directives/resizable.js
+++ res/app/js/directives/resizable.js
@@ -8,15 +8,58 @@ define([
 
             restrict:'A',
             link: function(scope, element, att) {
-                element.resizable(
-                    {
-                        handles: "n, e, s, w, nw, ne, sw,se"
+                var item = SetAttributeInformation(att.id).attributeInformation;
+
+                // 캔버스
+                if(att.id === 'canvas-content'){
+                    element.resizable(
+                        {
+                            handles: "n, e, s, w, nw, ne, sw,se"
+                        }
+                    );
+                }
+                else{
+                    element.resizable(
+                        {
+                            containment: '#canvas-content',
+                            handles: "n, e, s, w, nw, ne, sw,se"
+                        }
+                    );
+                }
+
+                element.bind('mousedown', function (){
+                    var item = SetAttributeInformation(att.id).attributeInformation;
+                    // 캔버스
+                    if(att.id === 'canvas-content'){
+                        // resize min width, height 지정
+                        var minHeight = 0, minWidth = 0;
+                        for(var key in item.childArr[0]){
+                            var child = item.childArr[0][key];
+
+                            var compareY = child.pos.y + child.size.height + Number(child.outline.weight)*2;
+                            if(minHeight<compareY){
+                                minHeight = compareY
+                            }
+
+                            var compareX = child.pos.x + child.size.width + Number(child.outline.weight)*2;
+                            if(minWidth<compareX){
+                                minWidth = compareX
+                            }
+                        }
+
+                        element.resizable(
+                            {
+                                handles: "n, e, s, w, nw, ne, sw,se",
+                                minHeight: minHeight,
+                                minWidth: minWidth
+                            }
+                        );
                     }
-                );
+                });
+
 
                 element.bind('mouseup', function (){
                     // 사이즈 업데이트
-                    var item = SetAttributeInformation(att.id).attributeInformation;
                     if(item){
                         item.size = {width: element.width(), height: element.height()};
                         if (item.state != 'new') {
