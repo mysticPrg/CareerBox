36556e5d2ed687611e8cad0a8f3ce558ccf3706f
JEONGBORAM-PC-W1 <shelter.sune@gmail.com>
2014-12-26 19:19:43 +0900

    [보람] 기본 템플릿 적용

---
+++ res/app/js/services/getTemplateListBasic.js
@@ -0,0 +1,22 @@
+/**
+ * Created by JEONGBORAM-PC-W1 on 2014-12-26.
+ */
+define(['app'], function (app) {
+    app.factory('getTemplateListBasic', function () {
+        return function ($http, infoType, callback) {
+            var url = 'http://210.118.74.166:8123/template/basic';
+            if(infoType !== ''){
+                url = url + '/' + infoType;
+            }
+
+            $http({
+                method: 'GET',
+                url: url,
+                responseType: 'json',
+                withCredentials: true
+            }).success(function (data) {
+                callback(data);
+            });
+        }
+    });
+});
