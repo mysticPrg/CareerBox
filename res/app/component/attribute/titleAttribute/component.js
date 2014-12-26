2d68372b375e704d0ec892f751cad2d11c01ef2c
ByoungJin <kbj2043@gmail.com>
2014-12-27 00:24:52 +0900

    [병진] 템플릿 수정에서 제목을 바꿀수 있도록 함

--- res/app/component/attribute/titleAttribute/component.js
+++ res/app/component/attribute/titleAttribute/component.js
@@ -1,15 +1,23 @@
 
 define([
-    'app'
+    'app',
+    'service/EditorData'
+
 ], function (app) {
 
-    app.directive('titleAttribute', function () {
+    app.directive('titleAttribute', function (EditorData) {
         return {
             restrict: 'A',
             scope: {
                 data : "=to"
             },
-            templateUrl: require.toUrl('component/attribute/titleAttribute/template.html')
+            templateUrl: require.toUrl('component/attribute/titleAttribute/template.html'),
+            link : function($scope, element, att){
+                // 예외처리
+                if((window.location.href.indexOf('TemplateEditor') >= 0 && att.id == 'canvas-content')){
+                    $scope.data.title = $scope.EditorData = EditorData.template.title
+                }
+            }
         };
     });
 
