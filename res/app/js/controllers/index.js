604c826b1e7d816622691448d104a6de58e1bb3d
mysticPrg <mysticPrg@gmail.com>
2014-12-30 04:47:51 +0900

    [현구] 메뉴 애니메이션 변경

--- res/app/js/controllers/index.js
+++ res/app/js/controllers/index.js
@@ -16,56 +16,16 @@ define([
             });
         };
 
-        var zoomImgArr = [
-            $('#zoomImg_1'),
-            $('#zoomImg_2'),
-            $('#zoomImg_3')
-        ];
-
-        function zoomIn(target) {
-            target.stop().animate({
-                width: '182px',
-                height: '212px'
-            }, 200, 'swing');
-        }
-        function zoomOut(target) {
-            target.stop().animate({
-                width: '122px',
-                height: '152px'
-            }, 200, 'swing');
-        }
-        function zoomOri(target) {
-            target.stop().animate({
-                width: '142px',
-                height: '172px'
-            }, 200, 'swing');
-        }
-        function zoomOriAll() {
-            zoomOri(zoomImgArr[0]);
-            zoomOri(zoomImgArr[1]);
-            zoomOri(zoomImgArr[2]);
-        }
-
-        zoomImgArr[0].on('mouseover', function() {
-            zoomIn(zoomImgArr[0]);
-            zoomOut(zoomImgArr[1]);
-            zoomOut(zoomImgArr[2]);
+        $('.overImg').on('mouseover', function (e) {
+            $(e.target).stop().animate({
+                top: '-30px'
+            }, 200);
         });
-        zoomImgArr[1].on('mouseover', function() {
-            zoomOut(zoomImgArr[0]);
-            zoomIn(zoomImgArr[1]);
-            zoomOut(zoomImgArr[2]);
+        $('.overImg').on('mouseout', function (e) {
+            $(e.target).stop().animate({
+                top: '0px'
+            }, 200);
         });
-        zoomImgArr[2].on('mouseover', function() {
-            zoomOut(zoomImgArr[0]);
-            zoomOut(zoomImgArr[1]);
-            zoomIn(zoomImgArr[2]);
-        });
-
-        var i;
-        for (i=0 ; i<3 ; i++) {
-            zoomImgArr[i].on('mouseout', zoomOriAll);
-        }
 
         $('#sampleBtn1').on('mouseover', function () {
             $('#samplePaperImg_1').stop().fadeIn('fast');
