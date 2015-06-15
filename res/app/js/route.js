52bf99a0c51ad5cd73c81d016093ac50fb61e5af
myticPrg <mysticPrg@gmail.com>
2015-06-15 20:01:51 +0900

    [현구] 대소문자 오타 수정 (PaperEditor -> paperEditor)

--- res/app/js/route.js
+++ res/app/js/route.js
@@ -11,7 +11,7 @@ define([
         function ($routeProvider) {
             $routeProvider
                 .when('/PaperEditor', {
-                    templateUrl: 'PaperEditor.html',
+                    templateUrl: 'paperEditor.html',
                     controller: 'PaperEditorController'
                 })
 //                .when('/SectionEditor', {
