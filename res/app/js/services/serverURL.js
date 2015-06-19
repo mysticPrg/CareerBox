f99b731d7f989b62ba613d482da65388476b2921
Boram-Jeong <shelter.sune@gmail.com>
2015-06-19 14:38:56 +0900

    [boram] change serverURL

--- res/app/js/services/serverURL.js
+++ res/app/js/services/serverURL.js
@@ -2,5 +2,5 @@
  * Created by gimbyeongjin on 14. 10. 23..
  */
 define(['app'], function(app) {
-    app.value('serverURL', 'http://104.155.200.239:8124');
+    app.value('serverURL', 'http://52.11.167.30:8124');
 });
--- src/util/serverURL.js
+++ src/util/serverURL.js
@@ -2,6 +2,6 @@
  * Created by myticPrg on 2015-06-15.
  */
 
-var serverURL = 'http://104.155.200.239:8124';
+var serverURL = ' http://52.11.167.30:8124';
 
 module.exports = serverURL;
